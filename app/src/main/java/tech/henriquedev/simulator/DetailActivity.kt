4afc5cbfa97d6e3a257bf871a743da0ff1953282
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-19 23:11:43 -0300

    layout details matches

---
+++ app/src/main/java/tech/henriquedev/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package tech.henriquedev.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.henriquedev.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
