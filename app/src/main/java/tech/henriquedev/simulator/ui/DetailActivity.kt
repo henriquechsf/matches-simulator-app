2e06abca908b2b72552b3260724aa3beea00791c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-24 23:42:37 -0300

    binding data into screen details

--- app/src/main/java/tech/henriquedev/simulator/ui/DetailActivity.kt
+++ app/src/main/java/tech/henriquedev/simulator/ui/DetailActivity.kt
@@ -2,7 +2,9 @@ package tech.henriquedev.simulator.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import tech.henriquedev.simulator.databinding.ActivityDetailBinding
+import tech.henriquedev.simulator.domain.Match
 
 class DetailActivity : AppCompatActivity() {
 
@@ -20,5 +22,26 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchFromExtra()
+    }
+
+    private fun loadMatchFromExtra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let { match ->
+            Glide.with(this).load(match.place.image).into(binding.ivPlace)
+            supportActionBar?.title = match.place.name
+
+            binding.tvDescription.text = match.description
+
+            Glide.with(this).load(match.homeTeam.image).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = match.homeTeam.name
+            binding.rbHomeTeamStars.rating = match.homeTeam.stars.toFloat()
+            if (match.homeTeam.score != null) binding.tvHomeTeamScore.text = match.homeTeam.score.toString()
+
+            Glide.with(this).load(match.awayTeam.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = match.awayTeam.name
+            binding.rbAwayTeamStars.rating = match.awayTeam.stars.toFloat()
+            if (match.awayTeam.score != null) binding.tvAwayTeamScore.text = match.awayTeam.score.toString()
+         }
     }
 }
